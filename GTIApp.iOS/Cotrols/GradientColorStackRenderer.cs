cfb164d4b0e4dff102bf410008f07a6efe6d794c
Carlos Mendez <carlos_mendez_10@live.com>
2019-08-28 17:04:44 -0600

    Design

---
+++ GTIApp.iOS/Cotrols/GradientColorStackRenderer.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Foundation;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+using gradient;
+using CoreGraphics;
+using CoreAnimation;
+using GTIApp.iOS.Cotrols;
+
+[assembly: ExportRenderer(typeof(GradientColorStack), typeof(GradientColorStackRenderer))]
+namespace GTIApp.iOS.Cotrols
+{
+
+    public class GradientColorStackRenderer : VisualElementRenderer<StackLayout>
+    {
+        public static CGColor ToCGColor(Color color)
+        {
+            return new CGColor(CGColorSpace.CreateSrgb(), new nfloat[] { (float)color.R, (float)color.G, (float)color.B, (float)color.A });
+        }
+        public override void Draw(CGRect rect)
+        {
+            base.Draw(rect);
+            GradientColorStack stack = (GradientColorStack)this.Element;
+
+            //CGColor startColor = stack.StartColor.ToCGColor();
+            //CGColor endColor = stack.EndColor.ToCGColor();
+
+            CGColor startColor = ToCGColor(stack.StartColor);
+            CGColor endColor = ToCGColor(stack.EndColor);
+
+            #region for Vertical Gradient
+
+            //var gradientLayer = new CAGradientLayer();
+
+            #endregion
+
+            #region for Horizontal Gradient
+
+            var gradientLayer = new CAGradientLayer()
+            {
+                StartPoint = new CGPoint(0, 0.5),
+                EndPoint = new CGPoint(1, 0.5)
+            };
+
+            #endregion
+
+            gradientLayer.Frame = rect;
+            gradientLayer.Colors = new CGColor[] { startColor, endColor };
+
+            NativeView.Layer.InsertSublayer(gradientLayer, 0);
+        }
+    }
+}
---
+++ GTIApp/View/Controls/GradientColorStack.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace gradient
+{
+    public class GradientColorStack : StackLayout
+    {
+        //public Color StartColor { get; set; }
+        //public Color EndColor { get; set; }
+        public static readonly BindableProperty StartColorProperty = BindableProperty.Create("StartColor",
+                                                typeof(Color),
+                                                typeof(GradientColorStack),
+                                               Color.Default,
+                                                BindingMode.TwoWay,
+                                                propertyChanged: StartColorPropertyChanged);
+
+        public Color StartColor
+        {
+            get { return (Color)GetValue(StartColorProperty); }
+            set { SetValue(StartColorProperty, value); }
+        }
+
+        private static void StartColorPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            if (newValue != null)
+            {
+                GradientColorStack stack = (GradientColorStack)bindable;
+                stack.StartColor = (Color)newValue;
+            }
+        }
+
+        public static readonly BindableProperty EndColorProperty = BindableProperty.Create("EndColor",
+                                                typeof(Color),
+                                                typeof(GradientColorStack),
+                                                Color.Default,
+                                                BindingMode.TwoWay,
+                                                propertyChanged: EndColorPropertyChanged);
+
+        public Color EndColor
+        {
+            get { return (Color)GetValue(EndColorProperty); }
+            set { SetValue(EndColorProperty, value); }
+        }
+
+        private static void EndColorPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            if (newValue != null)
+            {
+                GradientColorStack stack = (GradientColorStack)bindable;
+                stack.EndColor = (Color)newValue;
+            }
+        }
+    }
+}
+
